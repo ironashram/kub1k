e4b03d40bc0455eb8a338d9f3adc0fb37cd17ba3
Michele Palazzi <sysdadmin@m1k.cloud>
2023-07-07 12:36:48 +0200

    migrate to s3 backend
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+﻿/*****************************
+  S3 State Configuration
+*****************************/
+terraform {
+  backend "s3" {
+    region                      = "us-west-1"
+    endpoint                    = "s3.lab.m1k.cloud"
+    bucket                      = "terraform-backend"
+    key                         = "terraform.tfstate"
+    workspace_key_prefix        = "infra-mgmt-cluster"
+    force_path_style            = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
