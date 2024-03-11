8c8c1ad83863334f07b4a4e35063e6aad0efbe8d
Michele Palazzi <sysdadmin@m1k.cloud>
2024-03-12 00:36:38 +0100

    try another way
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,11 +1,14 @@
 locals {
-  // try to read the kubeconfig either from home or $GITHUB_WORKSPACE
-  kube_config_output = try(
-    pathexpand("~/.kube/config-files/${terraform.workspace}.yaml"),
-    pathexpand("$GITHUB_WORKSPACE/.kube/config-files/${terraform.workspace}.yaml")
-  )
+  kube_config_path_home      = "~/.kube/config-files/${terraform.workspace}.yaml"
+  kube_config_path_workspace = "$GITHUB_WORKSPACE/.kube/config-files/${terraform.workspace}.yaml"
 
-  kubeconfig             = try(yamldecode(file(local.kube_config_output)), null)
+  kubeconfig_home      = try(yamldecode(file(pathexpand(local.kube_config_path_home))), null)
+  kubeconfig_workspace = try(yamldecode(file(pathexpand(local.kube_config_path_workspace))), null)
+
+  kubeconfig = coalesce(local.kubeconfig_home, local.kubeconfig_workspace)
+
+  kube_config_output = local.kubeconfig == local.kubeconfig_home ? local.kube_config_path_home : local.kube_config_path_workspace
+
   cluster_host           = try(local.kubeconfig.clusters[0].cluster.server, null)
   cluster_ca_certificate = try(base64decode(local.kubeconfig.clusters[0].cluster.certificate-authority-data), null)
   client_certificate     = try(base64decode(local.kubeconfig.users[0].user.client-certificate-data), null)
