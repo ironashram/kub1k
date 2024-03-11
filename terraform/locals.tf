6d0b6a0904671d70529d668452b885d7adcafb40
Michele Palazzi <sysdadmin@m1k.cloud>
2024-03-12 00:59:00 +0100

    print
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,8 +2,8 @@ locals {
   kube_config_path_home      = "~/.kube/config-files/${terraform.workspace}.yaml"
   kube_config_path_workspace = "$GITHUB_WORKSPACE/.kube/config-files/${terraform.workspace}.yaml"
 
-  kubeconfig_home      = try(yamldecode(file(pathexpand(local.kube_config_path_home))), null)
-  kubeconfig_workspace = try(yamldecode(file(pathexpand(local.kube_config_path_workspace))), null)
+  kubeconfig_home      = try(yamldecode(file(pathexpand(local.kube_config_path_home))), "failed to read home kubeconfig")
+  kubeconfig_workspace = try(yamldecode(file(pathexpand(local.kube_config_path_workspace))), "failed to read workspace kubeconfig")
 
   kubeconfig = coalesce(local.kubeconfig_home, local.kubeconfig_workspace)
 
@@ -14,7 +14,6 @@ locals {
   client_certificate     = try(base64decode(local.kubeconfig.users[0].user.client-certificate-data), null)
   client_key             = try(base64decode(local.kubeconfig.users[0].user.client-key-data), null)
 }
-
 
 resource "null_resource" "print_paths" {
   provisioner "local-exec" {
