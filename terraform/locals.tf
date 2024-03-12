8683fca80b717a024642aaa13058bb631448e861
Michele Palazzi <sysdadmin@m1k.cloud>
2024-03-12 01:18:01 +0100

    wip
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
   kube_config_path_home      = "~/.kube/config-files/${terraform.workspace}.yaml"
-  kube_config_path_workspace = "$GITHUB_WORKSPACE/.kube/config-files/${terraform.workspace}.yaml"
+  kube_config_path_workspace = "${var.GITHUB_WORKSPACE}/.kube/config-files/${terraform.workspace}.yaml"
 
   kubeconfig_home      = try(yamldecode(file(pathexpand(local.kube_config_path_home))), "")
   kubeconfig_workspace = try(yamldecode(file(pathexpand(local.kube_config_path_workspace))), "bb")
@@ -13,13 +13,4 @@ locals {
   cluster_ca_certificate = try(base64decode(local.kubeconfig.clusters[0].cluster.certificate-authority-data), null)
   client_certificate     = try(base64decode(local.kubeconfig.users[0].user.client-certificate-data), null)
   client_key             = try(base64decode(local.kubeconfig.users[0].user.client-key-data), null)
-}
-
-resource "null_resource" "print_paths" {
-  provisioner "local-exec" {
-    command = "echo Home path: ${pathexpand(local.kube_config_path_home)}; echo Workspace path: ${pathexpand(local.kube_config_path_workspace)}"
-  }
-  triggers = {
-    always_run = "${timestamp()}"
-  }
 }
