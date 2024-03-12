c30bbe583918ca20b8bd6440c760f168172f494c
Michele Palazzi <sysdadmin@m1k.cloud>
2024-03-12 01:07:18 +0100

    fucking hell
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,12 +2,12 @@ locals {
   kube_config_path_home      = "~/.kube/config-files/${terraform.workspace}.yaml"
   kube_config_path_workspace = "$GITHUB_WORKSPACE/.kube/config-files/${terraform.workspace}.yaml"
 
-  kubeconfig_home      = try(yamldecode(file(pathexpand(local.kube_config_path_home))), "failed to read home kubeconfig")
-  kubeconfig_workspace = try(yamldecode(file(pathexpand(local.kube_config_path_workspace))), "failed to read workspace kubeconfig")
+  kubeconfig_home      = try(yamldecode(file(pathexpand(local.kube_config_path_home))), null)
+  kubeconfig_workspace = try(yamldecode(file(pathexpand(local.kube_config_path_workspace))), null)
 
   kubeconfig = coalesce(local.kubeconfig_home, local.kubeconfig_workspace)
 
-  kube_config_output = local.kubeconfig == local.kubeconfig_home ? local.kube_config_path_home : local.kube_config_path_workspace
+  kube_config_output = local.kubeconfig == local.kubeconfig_home ? pathexpand(local.kube_config_path_home) : pathexpand(local.kube_config_path_workspace)
 
   cluster_host           = try(local.kubeconfig.clusters[0].cluster.server, null)
   cluster_ca_certificate = try(base64decode(local.kubeconfig.clusters[0].cluster.certificate-authority-data), null)
@@ -17,7 +17,7 @@ locals {
 
 resource "null_resource" "print_paths" {
   provisioner "local-exec" {
-    command = "echo Home path: ${local.kube_config_path_home}; echo Workspace path: ${local.kube_config_path_workspace}"
+    command = "echo Home path: ${pathexpand(local.kube_config_path_home)}; echo Workspace path: ${pathexpand(local.kube_config_path_workspace)}"
   }
   triggers = {
     always_run = "${timestamp()}"
