1850c7a8cf850f9388a7fa5d70254290c6dff73d
Michele Palazzi <sysdadmin@m1k.cloud>
2024-03-19 23:20:47 +0100

    test
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">=2.10.1"
+      version = ">=2.12.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
