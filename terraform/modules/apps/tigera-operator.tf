84e899767a2939dce3ee35158cea7bd5080c0853
Michele Palazzi <sysdadmin@m1k.cloud>
2024-04-05 22:19:48 +0200

    chore: bump terraform managed helm charts
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/modules/apps/tigera-operator.tf
+++ terraform/modules/apps/tigera-operator.tf
@@ -7,7 +7,7 @@ resource "helm_release" "tigera_operator" {
 
   repository = "https://docs.projectcalico.org/charts"
   chart      = "tigera-operator"
-  version    = "v3.27.2"
+  version    = "v3.27.3"
   namespace  = "tigera-operator"
 
   create_namespace = true
