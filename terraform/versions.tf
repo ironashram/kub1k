11d02eba78f6266696a5c03cd8589ebb42f9f422
Michele Palazzi <sysdadmin@m1k.cloud>
2025-04-19 09:58:05 +0200

    chore: remove equality operator in versions.tf
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "=2.17.0"
+      version = "2.17.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -10,7 +10,7 @@ terraform {
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "=4.7.0"
+      version = "4.7.0"
     }
   }
 }
