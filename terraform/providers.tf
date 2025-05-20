d77dfa1f04219ebf8262df9c787b010d3b909fe0
Michele Palazzi <sysdadmin@m1k.cloud>
2025-05-20 22:25:03 +0200

    fix: set load_config_file to false in kubectl provider configuration
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -21,4 +21,5 @@ provider "kubectl" {
   cluster_ca_certificate = local.cluster_ca_certificate
   client_certificate     = local.client_certificate
   client_key             = local.client_key
+  load_config_file       = false
 }
