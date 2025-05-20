bf85cbb941706848563811eab0232017de3740f4
Michele Palazzi <sysdadmin@m1k.cloud>
2025-05-20 22:22:55 +0200

    refactor: remove unused lifecycle and resource definitions in crds.tf
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/modules/apps/crds.tf
+++ terraform/modules/apps/crds.tf
@@ -5,20 +5,4 @@ resource "helm_release" "crds" {
   namespace         = "default"
   dependency_update = true
 
-  lifecycle {
-    replace_triggered_by = [null_resource.src_crds]
-  }
-}
-
-data "archive_file" "crds" {
-
-  type        = "zip"
-  source_dir  = "${path.root}/../charts/crds"
-  output_path = "${path.root}/.terraform/crds.zip"
-}
-
-resource "null_resource" "src_crds" {
-  triggers = {
-    src_sha = data.archive_file.crds.output_sha
-  }
 }
