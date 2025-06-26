6cd412b59af8d16325829d27134b9a5bff4f0443
Michele Palazzi <sysdadmin@m1k.cloud>
2025-06-26 22:07:36 +0200

    chore: remove explicit CoreDNS servers and update Helm provider
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "3.0.1"
+      version = "3.0.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
