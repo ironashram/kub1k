45205fbb15391eee2a52f208e1e1183e306d544c
Michele Palazzi <sysdadmin@m1k.cloud>
2026-04-10 14:35:08 +0200

    fix: add missing Kubernetes service environment variables in kube-vip
    Signed-off-by: Michele Palazzi <sysdadmin@m1k.cloud>

--- terraform/modules/apps/kube-vip.tf
+++ terraform/modules/apps/kube-vip.tf
@@ -14,14 +14,16 @@ resource "helm_release" "kube_vip" {
         address = var.control_plane_vip
       }
       env = {
-        vip_interface      = "eth0"
-        vip_arp            = "true"
-        cp_enable          = "true"
-        vip_leaderelection = "true"
-        vip_leasename      = "plndr-cp-lock"
-        lb_enable          = "false"
-        svc_enable         = "false"
-        lb_port            = "6443"
+        vip_interface           = "eth0"
+        vip_arp                 = "true"
+        cp_enable               = "true"
+        vip_leaderelection      = "true"
+        vip_leasename           = "plndr-cp-lock"
+        lb_enable               = "false"
+        svc_enable              = "false"
+        lb_port                 = "6443"
+        KUBERNETES_SERVICE_HOST = "127.0.0.1"
+        KUBERNETES_SERVICE_PORT = "6443"
       }
       nodeSelector = {
         "node-role.kubernetes.io/control-plane" = "true"
